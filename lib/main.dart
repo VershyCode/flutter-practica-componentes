def8e55a322168affa0972d760bbd21fbebe8c7c
Paul G <paulsystem32@gmail.com>
2020-06-30 15:42:02 -0600

    Inputs - DatetimePicker
    Cambio de idioma en datetimepick

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:components/src/pages/alert_page.dart';
 import 'package:components/src/routes/routes.dart';
  
@@ -8,6 +9,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'),
+        const Locale('es', 'ES')
+      ],
       title: 'Components App',
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
