7dd0561c8db79927d3ab665690670586b6962792
Paul G <paulsystem32@gmail.com>
2020-06-30 01:50:26 -0600

    Practica 5 - FutureBldr
    Se utilizo el FutureBuilder para leer el json
    En un metodo asincrono que retorna un future.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,8 +2,6 @@ import 'package:components/src/providers/menu_provider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,17 +11,26 @@ class HomePage extends StatelessWidget {
       body: _lista(),
     );
   }
-
   Widget _lista() {
-    menuProvider.cargarData();
-    return ListView(
-      children: _listaItems(),
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot){
+        return ListView(
+          children: _listaItems(snapshot.data),
+        );
+      },
     );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile(title: Text('Hola mundo!'),)
-    ];
+  List<Widget> _listaItems(List<dynamic> data) {
+    return data.map((e) => Column(
+      children: <Widget>[
+        ListTile(
+          title: Text(e['texto']),
+        ),
+        Divider(),
+      ],
+    )).toList();
   }
 }
