4a1d12e0a679e84230bf7e4a651983a4860e060d
Paul G <paulsystem32@gmail.com>
2020-06-29 23:11:56 -0600

    Practica 3 - Nueva HomePage
    Se creo una nueva HomePage para comenzar a practicar
    Con recursos como lo son los json.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:components/src/pages/home_temp.dart';
 import 'package:flutter/material.dart';
+import 'package:components/src/pages/home_page.dart';
+// import 'package:components/src/pages/home_temp.dart';
  
 void main() => runApp(MyApp());
  
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
-      home: HomePageTemp()
+      home: HomePage()
     );
   }
 }
