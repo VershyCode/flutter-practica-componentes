20bd9be2256636617ce10d46bc05563e1574bac2
Paul G <paulsystem32@gmail.com>
2020-06-30 14:25:52 -0600

    Practica 11 - AnimatedContainer
    Se hizo uso del animated container para dibujar
    en la pantalla dinamicamente con efectos y accion
    retardada (duration, curve).
    Se creo el metodo cambiarForma() para cambiar el tamaño
    y tambien el color dinamicamente a base de numeros random.

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,3 +1,4 @@
+import 'package:components/src/pages/animated_container.dart';
 import 'package:flutter/material.dart';
 import 'package:components/src/pages/alert_page.dart';
 import 'package:components/src/pages/avatar_page.dart';
@@ -10,5 +11,6 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
     'alert' : (BuildContext context) => AlertPage(),
     'avatar' : (BuildContext context) => AvatarPage(),
     'card' : (BuildContext context) => CardPage(),
+    'animatedContainer' : (BuildContext context) => AnimatedContainerPage(),
   };
 }
