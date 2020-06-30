2b73a2efb8a5d5c0883215161c451f4a19e203e2
Paul G <paulsystem32@gmail.com>
2020-06-30 12:24:21 -0600

    Practica 9 - Alerts
    Se comenzo a elaborar una alerta (Alert Dialog).

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -12,6 +12,33 @@ class AlertPage extends StatelessWidget {
         child: Icon(Icons.close),
         onPressed: () => Navigator.pop(context),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _mostrarAlert(context),
+        ),
+      ),
+    );
+  }
+
+  void _mostrarAlert(BuildContext context){
+    showDialog(
+      context: context,
+      barrierDismissible: true, // Cerrar alerta con tap afuera.
+      builder: (context){
+        return AlertDialog(
+          title: Text('Titulo de la alerta'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Contenido de la alerta'),
+            ],
+          )
+        );
+      }
     );
   }
 }
