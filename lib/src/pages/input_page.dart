397375059a153f91b773eb34194944d429b8e481
Paul G <paulsystem32@gmail.com>
2020-06-30 14:51:37 -0600

    Inputs - Personalizacion
    Se creo un input y se personalizo, tambien
    dibujamos en la pantalla el length del value
    de dicho input y tambien su contenido.

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -8,12 +8,50 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  String _nombre = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Input page')
-      )
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        children: <Widget>[
+          _crearInput(),
+          Divider(),
+          _crearPersona(),
+          Divider()
+        ],
+      ),
     );
   }
+
+  Widget _crearInput() {
+    return TextField(
+      autofocus: false,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        counter: Text('Letras: ${_nombre.length}/15'),
+        hintText: 'Nombre de la persona',
+        labelText: 'Nombre',
+        helperText: 'Solo es el nombre',
+        suffixIcon: Icon(Icons.accessibility),
+        icon: Icon(Icons.account_circle)
+      ),
+      onChanged: (value){
+        setState(() {
+        _nombre = value;
+        });
+      },
+    );
+  }
+
+ Widget _crearPersona() {
+   return ListTile(
+     title: Text('Nombre es: $_nombre'),
+   );
+ }
 }
