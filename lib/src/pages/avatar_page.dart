097c461ad2b8c4519b1f13b26b273c025ba64c86
Paul G <paulsystem32@gmail.com>
2020-06-30 13:26:07 -0600

    Practica 10 - Avatar
    Se posiciono un avatar redondeado en un scaffold.

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,7 +7,29 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(4.5),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://dam.smashmexico.com.mx/wp-content/uploads/2018/05/24100112/Stan-Lee.jpg'),
+              radius: 24.3,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
       ),
+      body: Center(
+        child:FadeInImage(
+          image: NetworkImage('https://staticpasa2.cdnstatics2.com/static/upl/img//44MX07/sliderImgPrincipal_170_1-slider-png2-enciclopedia_marvel-584x584_1542305134542.png'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+        ),
+      )
     );
   }
 }
